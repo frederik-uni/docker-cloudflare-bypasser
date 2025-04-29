b3dc228a9be97d686890d2453130d158107008fb
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2025-04-29 12:05:49 +0200

    Add files via upload

--- cloudflare-bypasser.py
+++ cloudflare-bypasser.py
@@ -1,9 +1,8 @@
 import asyncio
-import os
-import subprocess
 import time
 
 import uvicorn
+import subprocess
 from botasaurus.browser import browser, Driver
 from botasaurus.window_size import WindowSize
 from botasaurus_humancursor import WebCursor
@@ -11,10 +10,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
-@browser(enable_xvfb_virtual_display=True, window_size=WindowSize.window_size_1920_1080)
+@browser(enable_xvfb_virtual_display=True, xvfb_extra_args=["-ac"], window_size=WindowSize.window_size_1920_1080)
 def calibrate(driver: Driver, data):
-    if not set_xauthority():
-        return
     import pyautogui
     driver.get("https://frederik-uni.github.io/click-recorder/")  # Assuming you're using Selenium's driver.get method
     screen_width, screen_height = pyautogui.size()
@@ -55,31 +52,15 @@ def check_for_refresh(driver, initial_url, initial_title, timeout=10, interval=0
     return False
 
 
-def set_xauthority():
-    xauthority_path = '/root/.Xauthority'
-    os.environ['XAUTHORITY'] = xauthority_path
+def click(x, y):
+    result = subprocess.run(["python", "clicker.py", str(int(x)), str(int(y))])
 
-    if not os.path.exists(xauthority_path):
-        try:
-            subprocess.run(['xauth', 'generate', ':0', '.', 'trusted'], check=True)
-            print(f"Generated .Xauthority file at {xauthority_path}")
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to generate .Xauthority file: {e}")
-        return False
-    return True
-
+    if result.returncode != 0:
+        print("Click process failed.")
 
 @browser(enable_xvfb_virtual_display=True)
 def bypass_cloudflare(driver: Driver, url):
-    if not set_xauthority():
-        print("Failed to generate .Xauthority(it fails when it first generates)")
-        if not set_xauthority():
-            print("Failed to generate .Xauthority")
-            return None
-    import pyautogui
     start_x, start_y = 10, 149
-    pyautogui.moveTo(start_x, start_y)
-    pyautogui.click()
 
     cursor = WebCursor(driver)
 
@@ -98,8 +79,7 @@ def bypass_cloudflare(driver: Driver, url):
 
     driver.enable_human_mode()
     cursor.move_mouse_to_point(x, y)
-    pyautogui.moveTo(start_x + x, start_y + y)
-    pyautogui.click()
+    click(start_x + x, start_y + y)
 
     driver.disable_human_mode()
     check_for_refresh(driver, url, title)
